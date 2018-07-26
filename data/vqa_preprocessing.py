18ba41840639916e7c0de5fe4f1df354a1d214aa
Varun Agrawal <varagrawal@gmail.com>
2018-07-26 15:47:40 -0400

    preprocessing code updated for Python 3 and VQA2

--- data/vqa_preprocessing.py
+++ data/vqa_preprocessing.py
@@ -12,25 +12,25 @@ import os
 import argparse
 
 def download_vqa():
-    os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip -P zip/')
-    os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip -P zip/')
-    os.system('wget http://visualqa.org/data/mscoco/vqa/Questions_Test_mscoco.zip -P zip/')
+    os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip -P zip/')
+    os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip -P zip/')
+    os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip -P zip/')
 
     # Download the VQA Annotations
-    os.system('wget http://visualqa.org/data/mscoco/vqa/Annotations_Train_mscoco.zip -P zip/')
-    os.system('wget http://visualqa.org/data/mscoco/vqa/Annotations_Val_mscoco.zip -P zip/')
+    os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip -P zip/')
+    os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip -P zip/')
 
 
     # Unzip the annotations
-    os.system('unzip zip/Questions_Train_mscoco.zip -d annotations/')
-    os.system('unzip zip/Questions_Val_mscoco.zip -d annotations/')
-    os.system('unzip zip/Questions_Test_mscoco.zip -d annotations/')
-    os.system('unzip zip/Annotations_Train_mscoco.zip -d annotations/')
-    os.system('unzip zip/Annotations_Val_mscoco.zip -d annotations/')
+    os.system('unzip zip/v2_Questions_Train_mscoco.zip -d annotations/')
+    os.system('unzip zip/v2_Questions_Val_mscoco.zip -d annotations/')
+    os.system('unzip zip/v2_Questions_Test_mscoco.zip -d annotations/')
+    os.system('unzip zip/v2_Annotations_Train_mscoco.zip -d annotations/')
+    os.system('unzip zip/v2_Annotations_Val_mscoco.zip -d annotations/')
 
 
 def main(params):
-    if params['download'] == 'True':
+    if params.download:
         download_vqa()
 
     '''
@@ -41,14 +41,14 @@ def main(params):
     test = []
     imdir='%s/COCO_%s_%012d.jpg'
 
-    if params['split'] == 1:
+    if params.split == 1:
 
-        print 'Loading annotations and questions...'
-        train_anno = json.load(open('annotations/mscoco_train2014_annotations.json', 'r'))
-        val_anno = json.load(open('annotations/mscoco_val2014_annotations.json', 'r'))
+        print('Loading annotations and questions...')
+        train_anno = json.load(open('annotations/v2_mscoco_train2014_annotations.json', 'r'))
+        val_anno = json.load(open('annotations/v2_mscoco_val2014_annotations.json', 'r'))
 
-        train_ques = json.load(open('annotations/MultipleChoice_mscoco_train2014_questions.json', 'r'))
-        val_ques = json.load(open('annotations/MultipleChoice_mscoco_val2014_questions.json', 'r'))
+        train_ques = json.load(open('annotations/v2_OpenEnded_mscoco_train2014_questions.json', 'r'))
+        val_ques = json.load(open('annotations/v2_OpenEnded_mscoco_val2014_questions.json', 'r'))
 
         subtype = 'train2014'
         for i in range(len(train_anno['annotations'])):
@@ -57,9 +57,8 @@ def main(params):
             image_path = imdir%(subtype, subtype, train_anno['annotations'][i]['image_id'])
 
             question = train_ques['questions'][i]['question']
-            mc_ans = train_ques['questions'][i]['multiple_choices']
 
-            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
         
         subtype = 'val2014'
         for i in range(len(val_anno['annotations'])):
@@ -68,17 +67,16 @@ def main(params):
             image_path = imdir%(subtype, subtype, val_anno['annotations'][i]['image_id'])
 
             question = val_ques['questions'][i]['question']
-            mc_ans = val_ques['questions'][i]['multiple_choices']
 
-            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans})
+            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
     else:
-        print 'Loading annotations and questions...'
+        print('Loading annotations and questions...')
         train_anno = json.load(open('annotations/mscoco_train2014_annotations.json', 'r'))
         val_anno = json.load(open('annotations/mscoco_val2014_annotations.json', 'r'))
 
-        train_ques = json.load(open('annotations/MultipleChoice_mscoco_train2014_questions.json', 'r'))
-        val_ques = json.load(open('annotations/MultipleChoice_mscoco_val2014_questions.json', 'r'))
-        test_ques = json.load(open('annotations/MultipleChoice_mscoco_test2015_questions.json', 'r'))
+        train_ques = json.load(open('annotations/v2_OpenEnded_mscoco_train2014_questions.json', 'r'))
+        val_ques = json.load(open('annotations/v2_OpenEnded_mscoco_val2014_questions.json', 'r'))
+        test_ques = json.load(open('annotations/v2_OpenEnded_mscoco_test2015_questions.json', 'r'))
         
         subtype = 'train2014'
         for i in range(len(train_anno['annotations'])):
@@ -87,9 +85,8 @@ def main(params):
             image_path = imdir%(subtype, subtype, train_anno['annotations'][i]['image_id'])
 
             question = train_ques['questions'][i]['question']
-            mc_ans = train_ques['questions'][i]['multiple_choices']
 
-            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
 
         subtype = 'val2014'
         for i in range(len(val_anno['annotations'])):
@@ -98,9 +95,8 @@ def main(params):
             image_path = imdir%(subtype, subtype, val_anno['annotations'][i]['image_id'])
 
             question = val_ques['questions'][i]['question']
-            mc_ans = val_ques['questions'][i]['multiple_choices']
 
-            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
+            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
         
         subtype = 'test2015'
         for i in range(len(test_ques['questions'])):
@@ -108,11 +104,10 @@ def main(params):
             image_path = imdir%(subtype, subtype, test_ques['questions'][i]['image_id'])
 
             question = test_ques['questions'][i]['question']
-            mc_ans = test_ques['questions'][i]['multiple_choices']
 
-            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans})
+            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question})
 
-    print 'Training sample %d, Testing sample %d...' %(len(train), len(test))
+    print('Training sample %d, Testing sample %d...' %(len(train), len(test)))
 
     json.dump(train, open('vqa_raw_train.json', 'w'))
     json.dump(test, open('vqa_raw_test.json', 'w'))
@@ -122,14 +117,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # input json
-    parser.add_argument('--download', default='False', help='Download and extract data from VQA server')
+    parser.add_argument('--download', action='store_true', default=False, help='Download and extract data from VQA server')
     parser.add_argument('--split', default=1, type=int, help='1: train on Train and test on Val, 2: train on Train+Val and test on Test')
   
     args = parser.parse_args()
-    params = vars(args)
-    print 'parsed input parameters:'
-    print json.dumps(params, indent = 2)
-    main(params)
+    print('parsed input parameters:')
+    print(json.dumps(vars(args), indent = 2))
+    main(args)
 
 
 
